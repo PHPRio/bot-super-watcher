46e8e3d94c4812df5e392a3db48e45ad61c36cbb
Vitor Mattos <vitor.mattos@phprio.org>
2019-01-22 18:43:00 -0200

    Ajuste para compatibilidade com environment do heroku

--- src/Db.php
+++ src/Db.php
@@ -16,11 +16,12 @@ class Db
             return $this->connection;
         }
         $config = new \Doctrine\DBAL\Configuration();
+        preg_match('\/\/(?P<user>[^:]*):(?P<pass>[^@]*)@(?P<host>[^:]*):(?P<port>[^\/]*)\/(?P<name>.*)', getenv('DATABASE_URL'), $matches);
         $this->connection = \Doctrine\DBAL\DriverManager::getConnection([
-            'dbname' => getenv('DB_NAME'),
-            'user' => getenv('DB_USER'),
-            'password' => getenv('DB_PASS'),
-            'host' => getenv('DB_HOST'),
+            'dbname' => $matches['name'],
+            'user' => $matches['user'],
+            'password' => $matches['password'],
+            'host' => $matches['host'],
             'driver' => 'pdo_pgsql',
         ], $config);
         return $this->connection;
